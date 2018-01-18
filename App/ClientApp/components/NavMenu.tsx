402c639754959d9be5fd9ff8a91141c6f8fd1a7f
George Stepanek <g.stepanek.nz@gmail.com>
2018-01-18 16:36:59 +1300

    Move Litters grid to Home page and add select/sort mock

--- App/ClientApp/components/NavMenu.tsx
+++ App/ClientApp/components/NavMenu.tsx
@@ -12,7 +12,7 @@ export class NavMenu extends React.Component<{}, {}> {
                         <span className='icon-bar'></span>
                         <span className='icon-bar'></span>
                     </button>
-                    <Link className='navbar-brand' to={ '/' }>App</Link>
+                    <Link className='navbar-brand' to={ '/' }>BOOP</Link>
                 </div>
                 <div className='clearfix'></div>
                 <div className='navbar-collapse collapse'>
@@ -25,11 +25,6 @@ export class NavMenu extends React.Component<{}, {}> {
                         <li>
                             <NavLink to={ '/counter' } activeClassName='active'>
                                 <span className='glyphicon glyphicon-education'></span> Counter
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={ '/fetchdata' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Fetch data
                             </NavLink>
                         </li>
                     </ul>
